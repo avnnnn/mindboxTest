d0be2ba3871f00a99648a4fd418064a3efdf522b
avnnnn <48981655+avnnnn@users.noreply.github.com>
2022-11-02 16:27:25 +0600

    Query

---
+++ Queries.sql
@@ -0,0 +1,29 @@
+﻿CREATE TABlE IF NOT EXISTS category(
+  id integer PRIMARY key AUTOINCREMENT,
+  name varchar(20)
+);
+
+
+CREATE TABlE IF NOT EXISTS product(
+  id integer PRIMARY KEY AUTOINCREMENT,
+  name varchar(20),
+  categoryId integer,
+  FOREIGN key(categoryId) REFERENCES category(id))
+
+INSERT INTO category(categoryname) VALUES("first");
+INSERT INTO category(categoryname) VALUES("second");
+INSERT INTO category(categoryname) VALUES("third");
+
+INSERT INTO product(productname, categoryid) VALUES("product1" ,1);
+INSERT INTO product(productname, categoryid) VALUES("product2" ,1);
+INSERT INTO product(productname, categoryid) VALUES("product3" ,1);
+INSERT INTO product(productname, categoryid) VALUES("product4" ,2);
+INSERT INTO product(productname, categoryid) VALUES("product5" ,2);
+INSERT INTO product(productname, categoryid) VALUES("product6" ,3);
+INSERT INTO product(productname) VALUES("product7");
+INSERT INTO product(productname) VALUES("product8");
+
+select productname as "Product Name", categoryname as "Category Name" 
+from product p 
+left join category c
+on p.categoryId = c.id
